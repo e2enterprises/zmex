6f317c81856c8362966ee03bb25c350c6600458a
Evan Purcer <ep@e2.gg>
2026-07-22 14:08:02 -0700

    get save/load story data loop working

--- src/main.rs
+++ src/main.rs
@@ -17,8 +17,7 @@ use itertools::Itertools;
 
 use encrusted_heart::options::Options;
 use encrusted_heart::traits::{BaseOutput, BaseUI};
-use encrusted_heart::zmachine::{Zmachine};
-// use encrusted_heart::zmachine::{Step, Zmachine};
+use encrusted_heart::zmachine::{Step, Zmachine};
 use encrusted_heart::zscii::ZChar;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
@@ -30,7 +29,6 @@ lazy_static! {
 }
 
 fn main() {
-    // https://docs.rs/clap/latest/clap/_tutorial/index.html
     let matches = Command::new("exiled")
         .version(VERSION)
         .about("A zmachine interpreter")
@@ -38,6 +36,7 @@ fn main() {
         .arg(Arg::new("STORY").required(true))
         .arg(Arg::new("INPUT").required(true).action(ArgAction::Append))
         .get_matches();
+    // https://docs.rs/clap/latest/clap/_tutorial/index.html
 
     let debug = matches.get_flag("debug");
     let story = matches.get_one::<String>("STORY").expect("Story file required.");
@@ -76,59 +75,48 @@ fn main() {
 
     let mut zvm = Zmachine::new(story_data, ui, opts);
 
-    // if matches.is_present("continue") {
-    //     let save_path = Path::new(matches.value_of("continue").unwrap());
-    //     if !save_path.is_file() {
-    //         println!(
-    //             "\nCouldn't find save file: \n   {}\n",
-    //             save_path.to_string_lossy()
-    //         );
-    //         process::exit(1);
-    //     }
-    //     let mut save_file = File::open(save_path).expect("Error opening save file");
-    //     let mut save_data = Vec::new();
-    //     save_file.read_to_end(&mut save_data).expect("Error reading save file");
-
-    //     // restore program counter position, stack frames, and dynamic memory
-    //     zvm.restore(&save_data);
-    // }
-
-    zvm.step();
-
-    for BaseOutput {
-        style: _,
-        content: text,
-    } in zvm.ui.drain_output() {
-        print!("{}", &text);
-        io::stdout().flush().unwrap();
-        continue;
-    }
-
-    if input.chars().count() == 1 {
-        zvm.handle_read_char(
-            ZChar::from_char(
-                input.chars().next().unwrap(), zvm.unicode_table()
-            ).unwrap()
-        );
-    } else {
-        zvm.handle_input(input);
-    }
-
-    zvm.step();
-
-    for BaseOutput {
-        style: _,
-        content: text,
-    } in zvm.ui.drain_output() {
-        print!("{}", &text);
-        io::stdout().flush().unwrap();
-        continue;
-    }
-
     let save_dir = story_path.parent().unwrap().to_string_lossy().into_owned();
     let save_name = story_path.file_stem().unwrap().to_string_lossy().into_owned();
     let mut save_path = PathBuf::from(&save_dir);
     save_path.push(save_name + "_save.quetzal");
+
+    if save_path.is_file() {
+        let mut save_file = File::open(&save_path).expect("Error opening save file");
+        let mut save_data = Vec::new();
+
+        save_file.read_to_end(&mut save_data).expect("Error reading save file");
+        zvm.load_savestate(&save_data);
+        // Note: zvm.restore panics here; zvm.load_savestate works instead.
+    }
+
+    let step = zvm.step();
+
+    match step {
+        Step::ReadChar => {
+            zvm.handle_read_char(
+                ZChar::from_char(
+                    input.chars().next().unwrap(), zvm.unicode_table()
+                ).unwrap()
+            );
+        },
+        Step::ReadLine => {
+            zvm.handle_input(input);
+        },
+        Step::Save(_) | Step::Restore | Step::Done => {
+            panic!("Error: Unexpected ZMachine state Step::{:?}", step);
+        }
+    }
+
+    zvm.step();
+
+    for BaseOutput {
+        style: _,
+        content: text,
+    } in zvm.ui.drain_output() {
+        print!("{}", &text);
+        io::stdout().flush().unwrap();
+        continue;
+    }
 
     let mut save_file;
     if let Ok(handle) = File::create(&save_path) {
