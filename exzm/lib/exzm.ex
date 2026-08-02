ba7e40044edb82e5127d96bf282c7d649f7073ff
Evan Purcer <ep@e2.gg>
2026-08-02 07:44:15 -0700

    [exzm] minor cleanup

--- exzm/lib/exzm.ex
+++ exzm/lib/exzm.ex
@@ -1,7 +1,7 @@
 defmodule Exzm.EncrustedNif do
   use Rustler, otp_app: :exzm, crate: "encrusted_nif"
 
-  def process_zmachine_input(_story_data, _state_data, _input_strings) do
+  def send_zmachine_input(_story_data, _state_data, _input_string) do
     :erlang.nif_error(:nif_not_loaded)
   end
 end
@@ -10,8 +10,8 @@ defmodule Exzm do
   alias Exzm.EncrustedNif
 
   # Suppress incorrect Dialyzer warnings due to NIF calls:
-  @dialyzer {:no_return, process_zmachine_inputs: 3}
-  @dialyzer {:no_return, process_zmachine_inputs_with_diagnostics: 3}
+  @dialyzer {:no_return, send_zmachine_inputs: 3}
+  @dialyzer {:no_return, send_zmachine_inputs_with_diagnostics: 3}
 
   # Parameters
   # ----------
@@ -52,10 +52,10 @@ defmodule Exzm do
 
     {save_data, output, diagnostics} =
       if include_diagnostics do
-        process_zmachine_input_with_diagnostics(story_data, <<>>, input)
+        send_zmachine_input_with_diagnostics(story_data, <<>>, input)
       else
         {save_data, output} =
-          process_zmachine_input(story_data, <<>>, input)
+          send_zmachine_input(story_data, <<>>, input)
 
         {save_data, output, nil}
       end
@@ -78,9 +78,9 @@ defmodule Exzm do
     inputs = [first_input | rest_inputs]
 
     if Keyword.get(opts, :diagnostics) do
-      process_zmachine_inputs_with_diagnostics(story_data, <<>>, inputs)
+      send_zmachine_inputs_with_diagnostics(story_data, <<>>, inputs)
     else
-      process_zmachine_inputs(story_data, <<>>, inputs)
+      send_zmachine_inputs(story_data, <<>>, inputs)
     end
   end
 
@@ -95,10 +95,10 @@ defmodule Exzm do
 
     {save_data, output, diagnostics} =
       if include_diagnostics do
-        process_zmachine_input_with_diagnostics(story_data, save_data, input)
+        send_zmachine_input_with_diagnostics(story_data, save_data, input)
       else
         {save_data, output} =
-          process_zmachine_input(story_data, save_data, input)
+          send_zmachine_input(story_data, save_data, input)
 
         {save_data, output, nil}
       end
@@ -122,9 +122,9 @@ defmodule Exzm do
     inputs = [first_input | rest_inputs]
 
     if Keyword.get(opts, :diagnostics) do
-      process_zmachine_inputs_with_diagnostics(story_data, save_data, inputs)
+      send_zmachine_inputs_with_diagnostics(story_data, save_data, inputs)
     else
-      process_zmachine_inputs(story_data, save_data, inputs)
+      send_zmachine_inputs(story_data, save_data, inputs)
     end
   end
 
@@ -136,34 +136,34 @@ defmodule Exzm do
     text |> String.trim() |> String.trim_trailing(">") |> String.trim_trailing()
   end
 
-  defp process_zmachine_input(story_data, save_data, input) do
+  defp send_zmachine_input(story_data, save_data, input) do
     {save_data, output} =
-      EncrustedNif.process_zmachine_input(story_data, save_data, input)
+      EncrustedNif.send_zmachine_input(story_data, save_data, input)
 
     output = format_output(output)
 
     {save_data, output}
   end
 
-  @dialyzer {:no_return, process_zmachine_inputs: 3}
-  defp process_zmachine_inputs(story_data, save_data, inputs) do
+  @dialyzer {:no_return, send_zmachine_inputs: 3}
+  defp send_zmachine_inputs(story_data, save_data, inputs) do
     [first_input | rest_inputs] = inputs
 
     {new_save_data, first_output} =
-      EncrustedNif.process_zmachine_input(story_data, save_data, first_input)
+      EncrustedNif.send_zmachine_input(story_data, save_data, first_input)
 
     {new_save_data, rest_output} =
-      process_zmachine_inputs(story_data, new_save_data, rest_inputs)
+      send_zmachine_inputs(story_data, new_save_data, rest_inputs)
 
     output = format_output(first_output <> rest_output)
 
     {new_save_data, output}
   end
 
-  defp process_zmachine_input_with_diagnostics(story_data, save_data, input) do
+  defp send_zmachine_input_with_diagnostics(story_data, save_data, input) do
     {nif_duration, {new_save_data, output}} =
       :timer.tc(
-        &EncrustedNif.process_zmachine_input/3,
+        &EncrustedNif.send_zmachine_input/3,
         [story_data, save_data, input]
       )
 
@@ -173,17 +173,17 @@ defmodule Exzm do
     {new_save_data, output, diagnostics}
   end
 
-  defp process_zmachine_inputs_with_diagnostics(story_data, save_data, inputs) do
+  defp send_zmachine_inputs_with_diagnostics(story_data, save_data, inputs) do
     [first_input | rest_inputs] = inputs
 
     {first_nif_duration, {new_save_data, first_output}} =
       :timer.tc(
-        &EncrustedNif.process_zmachine_input/3,
+        &EncrustedNif.send_zmachine_input/3,
         [story_data, save_data, first_input]
       )
 
     {new_save_data, rest_output, %{nif_duration_ms: rest_nif_duration_ms}} =
-      process_zmachine_inputs_with_diagnostics(story_data, new_save_data, rest_inputs)
+      send_zmachine_inputs_with_diagnostics(story_data, new_save_data, rest_inputs)
 
     output = format_output(first_output <> rest_output)
     diagnostics = %{nif_duration_ms: first_nif_duration / 1000 + rest_nif_duration_ms}
